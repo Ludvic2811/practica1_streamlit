32d851067a94e36d92588802736d8216dd603591
Luis Sambrano Mendoza <25460334@colima.tecnm.mx>
2025-11-28 13:05:22 -0600

    28 de noviembre

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hola Mundo!!")
+st.write("Esta es mi primera aplicación web creada con streamlit")
+st.subheader("¿Que hace esta aplicación")
+st.write(
+    """"
+    -Muestra un titulo en la parte superior
+    -Muestra un texto de bienvenida
+    -Corre como una app web en el navegador
+    """""
+)
+st.success("¡Felicidades! ya tiene tu primera app web con streamlit.")
